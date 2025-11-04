cd8299fcb4f323813898632d7968ce504dca1f04
Chloé Daunas <andr3amila@gmail.com>
2025-11-04 14:54:47 +0100

    premier exercice

--- rust_00/src/main.rs
+++ rust_00/src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "hello")]
+
+struct Args {
+    
+    #[arg(default_value = "World")]
+    name: String,
+
+    #[arg(long)]
+    upper:bool,
+
+    #[arg(long, default_value_t = 1)]
+    repeat: u8,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let greeting = if args.upper {
+        format!("HELLO, {}!", args.name.to_uppercase())
+    } else {
+        format!("Hello, {}!", args.name)
+    };
+
+    for _ in 0..args.repeat {
+        println!("{}", greeting);
+    }
 }
